e10e6fc2887bd4ba71373022995c9ec7df2f9831
Al-Abir <sheikhabir196@gmail.com>
2025-03-20 16:17:58 +0600

    Coustom Hook

---
+++ Coustom-Hook/coustom.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useCategory() {
+  const [categories, setCategories] = useState([]);
+
+  // get cat
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return categories;
+}
+
+// use 
+import useCategory form ".../../file ar name"
+
+const categories = useCategory()
